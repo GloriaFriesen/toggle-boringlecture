b22c6c7b412d70de449f9a8c3955ca0b4a4cc488
Christian Martinez and Gloria Friesen <gloria@>
2017-02-14 11:46:47 -0800

    add paragraph highlight border functionality

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(".box1").click(function(){
+    $(".box1").toggleClass("firstbox");
+
+  });
+  $(".box2").click(function(){
+    $(".box2").toggleClass("secondbox");
+  });
+});
